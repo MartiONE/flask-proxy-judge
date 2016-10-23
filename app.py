5acd46b16d11b7dc28ced833e53f8aff06541127
MartiONE <alej.marti@gmail.com>
2016-10-23 15:09:51 +0200

    Added HTTP_X_FORWARDED_FOR header to the mix, should work.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-a = ['HTTP_HOST', 'HTTP_CONNECTION','HTTP_UPGRADE_INSECURE_REQUESTS','HTTP_USER_AGENT', "HTTP_ACCEPT", 'REMOTE_ADDR', 'REMOTE_PORT', 'CLIENT_IP', 'REQUEST_METHOD', 'REQUEST_URI', 'REQUEST_TIME_FLOAT', 'REQUEST_TIME']
+a = ['HTTP_HOST', 'HTTP_CONNECTION','HTTP_UPGRADE_INSECURE_REQUESTS','HTTP_USER_AGENT', "HTTP_ACCEPT", 'REMOTE_ADDR', 'REMOTE_PORT', 'CLIENT_IP', 'REQUEST_METHOD', 'REQUEST_URI', 'REQUEST_TIME_FLOAT', 'REQUEST_TIME', 'HTTP_X_FORWARDED_FOR']
 
 
 @app.route("/")
 def judge():
-    return "<pre>"+"\n".join(["{}= {}".format(i,j) for i,j in request.headers.environ.items()])+"</pre>"
+    return "<pre>"+"\n".join(["{}= {}".format(i,j) for i,j in request.headers.environ.items() if i in a])+"</pre>"
 
 @app.route("/json")
 def json():
